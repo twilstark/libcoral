393efee8f9216475eb42e00e8567ebd8c1887e6f
Tony Wilson <81553379+twilstark@users.noreply.github.com>
2022-03-09 17:03:25 +0100

    Update lstpu.h

--- coral/examples/lstpu.h
+++ coral/examples/lstpu.h
@@ -1,14 +1,9 @@
 #ifndef LIBCORAL_CORAL_EXAMPLES_LSTPU_H_
 #define LIBCORAL_CORAL_EXAMPLES_LSTPU_H_
 
-#include <cstddef>
-#include <string>
-#include <unordered_map>
 
-namespace coral {
-
-  int Check_devices(void);
+int Check_devices(void);
 
 
-}  // namespace coral
+
 #endif  // LIBCORAL_CORAL_EXAMPLES_LSTPU_H_
